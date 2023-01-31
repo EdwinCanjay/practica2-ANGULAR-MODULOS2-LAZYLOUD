cc65b3ac47e29aebd3b370a9682f03d0383f1241
EdwinCanjay <canjayedwin105@gmail.com>
2023-01-31 08:14:38 -0600

    practica 2 modulos2

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+//import { ContactComponent } from './pages/contact/contact.component';
+//import { AboutComponent } from './pages/about/about.component';
+//import { HomeComponent } from './pages/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    /*ContactComponent,
+    AboutComponent,
+    HomeComponent,*/
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    PagesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
